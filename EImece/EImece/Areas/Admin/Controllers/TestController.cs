c9bf0e9fd6dfedd94929f2c71b6603bb30fdd78b
EMIN YUCE <eminyuce@gmail.com>
2016-12-05 16:23:31 -0500

    Angular Js DataTable development

---
+++ EImece/EImece/Areas/Admin/Controllers/TestController.cs
@@ -0,0 +1,58 @@
+﻿using EImece.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Linq.Dynamic;
+
+namespace EImece.Areas.Admin.Controllers
+{
+    public class TestController : BaseAdminController
+    {
+        // GET: Admin/Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult getData()
+        {
+            //Datatable parameter
+            var draw = Request.Form.GetValues("draw").FirstOrDefault();
+            //paging parameter
+            var start = Request.Form.GetValues("start").FirstOrDefault();
+            var length = Request.Form.GetValues("length").FirstOrDefault();
+            //sorting parameter
+            var sortColumn = Request.Form.GetValues("columns[" + Request.Form.GetValues("order[0][column]").FirstOrDefault() + "][name]").FirstOrDefault();
+            var sortColumnDir = Request.Form.GetValues("order[0][dir]").FirstOrDefault();
+            //filter parameter
+            var searchValue = Request.Form.GetValues("search[value]").FirstOrDefault();
+            List<Product> allCustomer = new List<Product>();
+            int pageSize = length != null ? Convert.ToInt32(length) : 0;
+            int skip = start != null ? Convert.ToInt32(start) : 0;
+            int recordsTotal = 0;
+            //Database query
+            var v = ProductRepository.GetAll();
+                //search
+                if (!string.IsNullOrEmpty(searchValue))
+                {
+                    v = v.Where(a =>
+                        a.ProductCode.Contains(searchValue) ||
+                        a.Name.Contains(searchValue) 
+                        );
+                }
+
+            //sort
+            if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDir)))
+            {
+                //for make sort simpler we will add Syste.Linq.Dynamic reference
+                v = v.OrderBy(sortColumn + " " + sortColumnDir);
+            }
+
+            recordsTotal = v.Count();
+                allCustomer = v.Skip(skip).Take(pageSize).ToList();
+
+            return Json(new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = allCustomer });
+        }
+    }
+}
--- EImece/EImece/Scripts/ng-script/MyApp.js
+++ EImece/EImece/Scripts/ng-script/MyApp.js
@@ -19,5 +19,30 @@
         }
     });
 
+
+    var app = angular.module('myApp', ['datatables']);
+    app.controller('homeCtrl', ['$scope', '$http', 'DTOptionsBuilder', 'DTColumnBuilder',
+        function ($scope, $http, DTOptionsBuilder, DTColumnBuilder) {
+            $scope.dtColumns = [
+                //here We will add .withOption('name','column_name') for send column name to the server 
+                DTColumnBuilder.newColumn("Id", "ID").withOption('name', 'Id'),
+                DTColumnBuilder.newColumn("Name", "Name").withOption('name', 'Name'),
+                DTColumnBuilder.newColumn("IsActive", "Is Active").withOption('name', 'IsActive'),
+                DTColumnBuilder.newColumn("Position", "Position").withOption('name', 'Position'),
+                DTColumnBuilder.newColumn("UpdatedDate", "Updated Date").withOption('name', 'UpdatedDate')
+            ]
+
+            $scope.dtOptions = DTOptionsBuilder.newOptions().withOption('ajax', {
+                dataSrc: "data",
+                url: "/test/getdata",
+                type: "POST"
+            })
+            .withOption('processing', true) //for show progress bar
+            .withOption('serverSide', true) // for server side processing
+            .withPaginationType('full_numbers') // for get full pagination options // first / last / prev / next and page numbers
+            .withDisplayLength(10) // Page size
+            .withOption('aaSorting', [0, 'asc']) // for default sorting column // here 0 means first column
+        }])
+
 })();
 
