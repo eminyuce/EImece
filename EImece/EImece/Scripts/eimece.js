12cb0bbb73c2ac54dd1d494f1feaba0e4ec55111
EMIN YUCE <eminyuce@gmail.com>
2017-03-13 10:25:56 -0400

    Navigation bug fixed

--- EImece/EImece/Scripts/eimece.js
+++ EImece/EImece/Scripts/eimece.js
@@ -1,1 +1,19 @@
-﻿ 
+﻿jQuery(function () {
+    $("#btn-search").click(function () {
+        console.log("eee");
+        $(".error").hide();
+        var hasError = false;
+        var searchReg = /^[a-zA-Z0-9-]+$/;
+        var searchVal = $("#Search_TextBox").val();
+        if (searchVal == '') {
+            $("#errorMessage").text($("#SearchRequiredErrorMessage").val());
+            //  $("#Search_TextBox").after('<span class="error">' + $("#SearchRequiredErrorMessage").val() + '</span>');
+            hasError = true;
+        } else if (!searchReg.test(searchVal)) {
+            $("#errorMessage").text($("#SearchValidText").val());
+            // $("#Search_TextBox").after('<span class="error">' + $("#SearchValidText").val() + '</span>');
+            hasError = true;
+        }
+        if (hasError == true) { return false; }
+    });
+});
