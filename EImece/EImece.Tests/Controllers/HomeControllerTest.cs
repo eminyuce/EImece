5abe676f2c7e4821dc6ea65b89c9293a89a1b280
EMIN YUCE <prisonerever@gmail.com>
2020-06-21 17:19:11 +0300

    Undo

--- EImece/EImece.Tests/Controllers/HomeControllerTest.cs
+++ EImece/EImece.Tests/Controllers/HomeControllerTest.cs
@@ -80,17 +80,7 @@ namespace EImece.Tests.Controllers
                     EmailSender emailSender = new EmailSender();
                 //    emailSender.SendEmail("eminyuce@gmail.com", "Test", "TESTING", emailAccount);
 
-                    var mail = new MailMessage();
-                    var SmtpServer = new SmtpClient("srvm05.trwww.com");
-                    mail.From = new MailAddress("test@erayweb.net");
-                    mail.To.Add("test@erayweb.net");
-                    mail.To.Add("eminyuce@gmail.com");
-                    mail.Subject = "Test Mail";
-                    mail.Body = "This is for testing SMTP mail";
-                    SmtpServer.Port = 465;
-                    SmtpServer.Credentials = new System.Net.NetworkCredential("test@erayweb.net", "W6y9BkcH");
-                    SmtpServer.EnableSsl = false;
-                    SmtpServer.Send(mail);
+                
 
                 }
             }
