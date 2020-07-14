41ef332fb8e9772a99df0973c4a9f76395041b7f
EMIN YUCE <eminyuce@gmail.com>
2020-07-15 00:37:05 +0300

    Shopping Cart

--- EImece/EImece/Controllers/PaymentController.cs
+++ EImece/EImece/Controllers/PaymentController.cs
@@ -1,10 +1,7 @@
-﻿using EImece.Domain.Entities;
-using EImece.Domain.Models.FrontModels;
-using EImece.Domain.Services;
+﻿using EImece.Domain.Models.FrontModels;
 using Iyzipay;
 using Iyzipay.Model;
 using Iyzipay.Request;
-using Ninject;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
