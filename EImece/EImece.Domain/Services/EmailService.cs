38decfd4303d6ea221ddffa755a12eab56e207d6
EMIN YUCE <eminyuce@gmail.com>
2020-07-23 18:42:20 +0300

    Move codes from core to domain

---
+++ EImece/EImece.Domain/Services/EmailService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EImece.Domain.Helpers.EmailHelper;
+using EImece.Domain.Services;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Ninject;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EImece.Domain.Helpers.EmailHelper;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Ninject;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using EImece.Domain;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace EImece.Domain.Services
+{
+    public class EmailService : IIdentityMessageService
+    {
+        [Inject]
+        public IEmailSender EmailSender { get; set; }
+
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Plug in your email service here to send an email.
+            EmailSender.SendEmail(message.Destination, message.Subject, message.Body);
+            return Task.FromResult(0);
+        }
+    }
+}
--- EImece/EImece.Domain/Services/SmsService.cs
+++ EImece/EImece.Domain/Services/SmsService.cs
@@ -1,6 +1,15 @@
-﻿using EImece.Domain.Helpers.EmailHelper;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EImece.Domain.Helpers.EmailHelper;
 using EImece.Domain.Services;
-using EImece.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
@@ -31,21 +40,8 @@ using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
-namespace EImece
+namespace EImece.Domain.Services
 {
-    public class EmailService : IIdentityMessageService
-    {
-        [Inject]
-        public IEmailSender EmailSender { get; set; }
-
-        public Task SendAsync(IdentityMessage message)
-        {
-            // Plug in your email service here to send an email.
-            EmailSender.SendEmail(message.Destination, message.Subject, message.Body);
-            return Task.FromResult(0);
-        }
-    }
-
     public class SmsService : IIdentityMessageService
     {
         public Task SendAsync(IdentityMessage message)
@@ -54,6 +50,4 @@ namespace EImece
             return Task.FromResult(0);
         }
     }
-
-  
 }
