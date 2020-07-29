c4c4cd1017e9a94123f6d7cfac23fd94e9828b73
EMIN YUCE <eminyuce@gmail.com>
2020-07-29 13:17:28 +0300

    Navigation

--- EImece/EImece.Domain/Entities/Menu.cs
+++ EImece/EImece.Domain/Entities/Menu.cs
@@ -1,8 +1,12 @@
-﻿using Resources;
+﻿using EImece.Domain.Helpers.Extensions;
+using Resources;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
 namespace EImece.Domain.Entities
 {
@@ -33,5 +37,39 @@ namespace EImece.Domain.Entities
         public List<Menu> Childrens { get; set; }
 
         public ICollection<MenuFile> MenuFiles { get; set; }
+
+        [NotMapped]
+        public string DetailPageLink
+        {
+            get
+            {
+
+                var p = MenuLink.Split("_".ToCharArray());
+                var parts = p.First().Split("-".ToCharArray());
+                var action = parts[1];
+                var controller = parts[0];
+                String mid =  p.Last();
+                string resultLink = "";
+                var urlHelper = new UrlHelper(HttpContext.Current.Request.RequestContext);
+                if (LinkIsActive && !String.IsNullOrEmpty(Link))
+                {
+                    resultLink = Link;
+                }
+                else if (controller.Equals("pages", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    resultLink = urlHelper.Action("detail", controller, new { id = this.GetSeoUrl() });
+                }
+                else if (controller.Equals("stories", StringComparison.InvariantCultureIgnoreCase)
+                                                            && action.Equals("categories", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    resultLink = urlHelper.Action(action, controller, new { id = mid });
+                }
+                else
+                {
+                    resultLink = urlHelper.Action(action, controller);
+                }
+                return resultLink;
+            }
+        }
     }
 }
