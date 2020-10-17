c3870a42f836397c2507392309e0aaf27d96d74e
EMIN YUCE <eminyuce@gmail.com>
2020-10-17 07:58:33 +0300

    Shopping Basket no product found

--- EImece/Resources/Resource.Designer.cs
+++ EImece/Resources/Resource.Designer.cs
@@ -2222,6 +2222,19 @@ namespace Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to &lt;h1 class=&quot;h2&quot;&gt; Alışveriş sepetinizde ürün bulunmuyor!&lt;/h1&gt;
+        ///                                &lt;p class=&quot;text-muted&quot;&gt;
+        ///                                    Beğendiğiniz ürünleri sepetinize ekleyerek sipariş verebilirsiniz. Alışveriş yapmak için
+        ///                                    &lt;a class=&quot;font-weight-semibold&quot; href=&quot;@Url.Action(&quot;Index&quot;,&quot;Home&quot;)&quot;&gt; tıklayın.&lt;/a&gt;&lt;br&gt;
+        ///                                &lt;/p&gt;.
+        /// </summary>
+        public static string NoProductFoundInShoppingBasket {
+            get {
+                return ResourceManager.GetString("NoProductFoundInShoppingBasket", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Bu kategoriye eklenmiş bir ürün yoktur..
         /// </summary>
         public static string NoProductsFoundForCategory {
