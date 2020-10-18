b7dae0b71c28e952b51dce29075fc82e82c1aead
EMIN YUCE <eminyuce@gmail.com>
2020-10-18 16:22:41 +0300

    Bugs fixed

--- EImece/Resources/AdminResource.Designer.cs
+++ EImece/Resources/AdminResource.Designer.cs
@@ -1096,7 +1096,7 @@ namespace Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Resim Boy (Tavsiye ölçü: 800 Px).
+        ///   Looks up a localized string similar to Resim Boy (Tavsiye: 800 Px).
         /// </summary>
         public static string ImageHeight {
             get {
@@ -1123,7 +1123,7 @@ namespace Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Resim En (Tavsiye ölçü: 800 Px).
+        ///   Looks up a localized string similar to Resim En (Tavsiye: 800 Px).
         /// </summary>
         public static string ImageWidth {
             get {
