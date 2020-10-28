f5e4e4a9de052559261e9085a8e395f8145220bf
EMIN YUCE <eminyuce@gmail.com>
2020-10-28 16:24:48 +0300

    Info Pages

--- EImece/EImece/Controllers/InfoController.cs
+++ EImece/EImece/Controllers/InfoController.cs
@@ -1,4 +1,5 @@
-﻿using EImece.Domain.Services.IServices;
+﻿using EImece.Domain;
+using EImece.Domain.Services.IServices;
 using Ninject;
 using System.Net;
 using System.Web.Mvc;
@@ -7,13 +8,20 @@ namespace EImece.Controllers
 {
     public class InfoController : BaseController
     {
+
+
         [Inject]
         public IMenuService MenuService { get; set; }
 
         // GET: Info
         public ActionResult Index(string id)
         {
-            var page = MenuService.GetPageByMenuLink("info-" + id, CurrentLanguage);
+            if (string.IsNullOrEmpty(id))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var page = MenuService.GetPageByMenuLink(Constants.INFO_PREFIX + id, CurrentLanguage);
             if (page == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.NotFound);
