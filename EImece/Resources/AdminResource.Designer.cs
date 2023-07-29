2ff3cd07fbab96e7bc7933a36bf177e35a695764
eminyuce <eminyuce@gmail.com>
2023-07-29 14:21:26 -0400

    Bugs fixed

--- EImece/Resources/AdminResource.Designer.cs
+++ EImece/Resources/AdminResource.Designer.cs
@@ -19,7 +19,7 @@ namespace Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class AdminResource {
@@ -210,6 +210,15 @@ namespace Resources {
         public static string AdminPanelLoginPage {
             get {
                 return ResourceManager.GetString("AdminPanelLoginPage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Toplamın sonucunu buraya yazınız.
+        /// </summary>
+        public static string AdminPanelLoginPageSumTotal {
+            get {
+                return ResourceManager.GetString("AdminPanelLoginPageSumTotal", resourceCulture);
             }
         }
         
