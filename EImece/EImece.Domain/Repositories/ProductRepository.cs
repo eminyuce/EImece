0e9b70519a28a16d17b8d202028823b5d561058b
eminyuce <eminyuce@gmail.com>
2023-08-11 19:21:25 -0400

    Product Search by product code

--- EImece/EImece.Domain/Repositories/ProductRepository.cs
+++ EImece/EImece.Domain/Repositories/ProductRepository.cs
@@ -138,7 +138,7 @@ namespace EImece.Domain.Repositories
             includeProperties.Add(r => r.MainImage);
             includeProperties.Add(r => r.ProductCategory);
             includeProperties.Add(r => r.ProductTags.Select(q => q.Tag));
-            Expression<Func<Product, bool>> match = r2 => r2.IsActive && r2.Lang == lang && r2.Name.Contains(search.Trim());
+            Expression<Func<Product, bool>> match = r2 => r2.IsActive && r2.Lang == lang && (r2.Name.Contains(search.Trim()) || (r2.ProductCode!=null && r2.ProductCode.Contains(search.Trim())));
 
             if (sorting == SortingType.LowHighPrice)
             {
