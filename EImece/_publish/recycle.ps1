03c8586c10bfdd581f62a727b4e35dd61dd50c88
EMIN YUCE <eminyuce@gmail.com>
2026-08-07 19:19:20 +0300

    remove _publish folder

--- EImece/_publish/recycle.ps1
+++
@@ -1,4 +0,0 @@
-﻿Import-Module WebAdministration
-Restart-WebAppPool -Name Eimece
-Start-Sleep -Seconds 2
-"recycled" | Set-Content "c:\Users\eminy\source\repos\eminyuce\EImece\EImece\_publish\recycle.log"
