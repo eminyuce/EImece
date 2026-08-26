ab6dea98dc39da9486ac383aea2d045774bb7322
EMIN YUCE <eminyuce@gmail.com>
2026-08-26 17:50:24 +0300

    feat(admin): add user audit reports with summary, monthly breakdown, stored procedures and filters

---
+++ EImece/EImece.Domain/Models/AdminModels/UserAuditReportViewModel.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Web.Mvc;
+
+namespace EImece.Domain.Models.AdminModels
+{
+    public class UserAuditReportViewModel
+    {
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string UserId { get; set; }
+        public string TableName { get; set; }
+        public string ActionType { get; set; }
+        public string ActiveTab { get; set; } = "summary";
+
+        public IList<SelectListItem> AvailableUsers { get; set; } = new List<SelectListItem>();
+        public IList<SelectListItem> AvailableTables { get; set; } = new List<SelectListItem>();
+        public IList<SelectListItem> AvailableActionTypes { get; set; } = new List<SelectListItem>();
+
+        public DataTable UserSummaryData { get; set; } = new DataTable();
+        public DataTable MonthlyBreakdownData { get; set; } = new DataTable();
+        public DataTable DetailedRecordsData { get; set; } = new DataTable();
+
+        public int TotalUsersCount { get; set; }
+        public int TotalCreatedCount { get; set; }
+        public int TotalUpdatedCount { get; set; }
+        public int TotalActivityCount { get; set; }
+    }
+
+    public class UserAuditItemDto
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+    }
+}
